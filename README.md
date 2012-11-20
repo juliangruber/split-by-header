1bac434c7be675c65df01c4c3bd4447ea73c782a
Julian Gruber <julian@juliangruber.com>
2012-11-20 18:49:23 +0100

    implementation working

--- README.md
+++ README.md
@@ -1,17 +1,15 @@
 
 # split-by-header
 
+Split binary streams by length fields in messages' headers
+
 ## Usage
 
-Splitting axon messages:
-
 ```js
-var split = require('split-by-length')
-
-var splitter = split({ type : 'UInt24BE', offset : 1})
+var split = require('split-by-header')
 
 binaryStream
-  .pipe(splitter)
+  .pipe(split({ type : 'UInt24BE', offset : 1 }))
   .pipe(process.stdout)
 ```
 
@@ -21,11 +19,15 @@ binaryStream
 
 `cfg` can be an object with those fields:
 
-* `type`: Type of the length value. Supported are {8,16,24,32} bit (un)signed Integers, Doubles and Floats in BE & LE
+* `type`: Type of the length value. Supported are {8,16,24,32} bit (un)signed Integers in BE & LE. See [node api](http://nodejs.org/api/buffer.html)
 * `offset`: Position to start reading from (optional)
 * `modifier`: the real length is returned by the modifier (optional)
 
-Or a string which sets `cfg.type`.
+Or a string which sets `cfg.type`:
+
+```js
+split('UInt24BE')
+```
 
 ## License
 
--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var Stream = require('stream')
 var inherits = require('util').inherits
 
+var lengths = {
+  'UInt8' : 1,
+  'Int8' : 1,
+  'UInt16LE' : 2,
+  'UInt16BE' : 2,
+  'Int16LE' : 2,
+  'Int16BE' : 2,
+  'UInt32LE' : 4,
+  'Int32BE' : 4,
+  'Int32LE' : 4,
+  'UInt32BE' : 4
+}
+
 module.exports = function (opts) {
   if (typeof opts == 'string') opts = { type : opts }
   if (!opts || !opts.type) throw new Error('no type provided')
@@ -18,38 +31,37 @@ function split(opts) {
   this.offset = opts.offset
   this.modifier = opts.modifier
 
-  this.len = 0
+  this.length = 0
   this.current = 0
 
   this.buf = null
-  this.bufPre = null
 
-  this.inMessage = false
-  this.beforeLen = true
+  this.lengthParsed = false
 }
 
 inherits(split, Stream)
 
 split.prototype.write = function (data) {
-  console.log('inMessage', this.inMessage, 'beforeLen', this.beforeLen)
-  if (!this.inMessage) {
-    if (this.beforeLen && data.length < lengths[this.type]) {
-      this.bufPre = new Buffer(data)
-      this.beforeLen = false
-      return
-    } else if (this.bufPre) {
+  if (!this.lengthParsed) {
+    if (this.buf) {
       var oldData = data
-      data = new Buffer(oldData.length + this.bufPre.length)
-      this.bufPre.copy(data)
-      oldData.copy(data, this.bufPre.length)
-      this.bufPre = null
+      var data = new Buffer(this.buf.length + oldData.length)
+      this.buf.copy(data)
+      oldData.copy(data, this.buf.length)
+      this.buf = null
     }
-    this.len = data['read' + this.type](this.offset + this.current)
-    this.inMessage = true
-  }
-  if (!this.buf) this.buf = new Buffer(this.len)
 
-  var remaining = this.len - this.current
+    if (data.length < this.offset + lengths[this.type]) return this.buf = data
+
+    this.length = data['read' + this.type](this.offset)
+    if (this.modifier) this.length = this.modifier(this.length)
+    this.lengthParsed = true
+  }
+
+  if (!this.buf) this.buf = new Buffer(this.length)
+
+  var remaining = this.length - this.current
+  //var n = Math.min(data.length, remaining) 
   var n = remaining > data.length
     ? data.length
     : remaining
@@ -57,30 +69,17 @@ split.prototype.write = function (data) {
   data.copy(this.buf, this.current, 0, n)
   this.current += n
 
-  if (this.current == this.len) {
-    this.inMessage = false
-    this.beforeLen = true
-    this.current = 0
+  if (this.current == this.length) {
     this.emit('data', this.buf)
-    this.buf = null
+    this.reset()
   }
 
   if (data.length - n) this.write(data.slice(n))
 }
 
-var lengths = {
-  'UInt8' : 1,
-  'Int8' : 1,
-  'UInt16LE' : 2,
-  'UInt16BE' : 2,
-  'Int16LE' : 2,
-  'Int16BE' : 2,
-  'UInt32LE' : 4,
-  'Int32BE' : 4,
-  'Int32LE' : 4,
-  'UInt32BE' : 4,
-  'FloatLE' : 4,
-  'FloatBE' : 4,
-  'DoubleLE' : 8,
-  'DoubleBE' : 8
+split.prototype.reset = function () {
+  this.lengthParsed = false
+  this.current = 0
+  this.buf = null
 }
+
